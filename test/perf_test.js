85e1d841d33e6e669c6535fca4ce7f76793fd69d
Eamonn O'Brien-Strain <eobrain@gmail.com>
2020-04-26 14:19:32 -0700

    Add test for how well the algorithm works.

---
+++ test/perf_test.js
@@ -0,0 +1,22 @@
+import test from 'ava'
+import maxichromeDev from '../src/node/index_dev.js'
+
+const kL = 0.5
+const kC = 8
+const kH = 2
+
+test('minimum deltaE', async t => {
+  const colors = await maxichromeDev(kL, kC, kH, 5)
+
+  let dEMin = Infinity
+  let dEMax = -Infinity
+  for (let i = 0; i < 5; ++i) {
+    for (let j = i + 1; j < 5; ++j) {
+      const dE = colors[i].deltaE(kL, kC, kH, colors[j])
+      dEMin = Math.min(dE, dEMin)
+      dEMax = Math.max(dE, dEMax)
+    }
+  }
+
+  t.true(dEMin < 60, `Expecting dEMin ${dEMin} to be < 60`)
+})
