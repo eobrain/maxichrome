db6ffe0da7ace57584e113e619340b45ae3c615b
Eamonn O'Brien-Strain <eobrain@gmail.com>
2020-04-26 13:47:52 -0700

    Demo showing both foreground and background swatches

--- src/web/demo.js
+++ src/web/demo.js
@@ -8,13 +8,13 @@ import maxichrome from '../web/index.js'
 */
 
 const run = async () => {
-  const colorCount = colorCountElement.value
+  const colorCount = Number(colorCountElement.value)
   const avoidColor = avoidColorElement.value
   const colors = await maxichrome(colorCount, [avoidColor])
-  while (swatchesAreaElement.children.length > colorCount) {
+  while (swatchesAreaElement.children.length > colorCount * 2) {
     swatchesAreaElement.lastElementChild.remove()
   }
-  while (swatchesAreaElement.children.length < colorCount) {
+  while (swatchesAreaElement.children.length < colorCount * 2) {
     const swatchElement = document.createElement('SPAN')
     swatchElement.setAttribute('class', 'swatch')
     swatchesAreaElement.appendChild(swatchElement)
@@ -23,6 +23,12 @@ const run = async () => {
     const swatchElement = swatchesAreaElement.children[i]
     swatchElement.style.setProperty('color', avoidColor)
     swatchElement.style.setProperty('background-color', color)
+    swatchElement.innerHTML = ' ' + color + ' '
+  })
+  colors.forEach((color, i) => {
+    const swatchElement = swatchesAreaElement.children[colorCount + i]
+    swatchElement.style.setProperty('background-color', avoidColor)
+    swatchElement.style.setProperty('color', color)
     swatchElement.innerHTML = ' ' + color + ' '
   })
 }
